e4507b8376ca790a89ac4467f7ddc278729a9f8b
Nastya <anastasiia.y.sitnikova@gmail.com>
2024-06-05 14:58:18 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const populationArray = [
+    ...document.querySelectorAll(".population")
+];
+const populationNums = [];
+populationArray.forEach((element)=>{
+    const elementData = element.innerHTML.replace(/,/g, "");
+    return populationNums.push(+elementData);
+});
+const totalPopulationNum = populationNums.reduce((acc, cur)=>acc + cur, 0);
+const averagePopulationNum = totalPopulationNum / populationNums.length;
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+totalPopulation.textContent = totalPopulationNum.toLocaleString("en-US");
+averagePopulation.textContent = averagePopulationNum.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
